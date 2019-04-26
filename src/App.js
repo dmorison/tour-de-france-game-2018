4c4aa44d90b00512e527f16f59c025547e7f6157
david.morison <david.morison@moneysavingexpert.com>
2019-04-26 16:09:08 +0100

    upgraded react and all packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 import startList from './startlist2018.json';
 
